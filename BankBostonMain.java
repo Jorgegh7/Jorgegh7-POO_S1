5c9efa48ae2b8e449aa5408a192b0c212be1c9d6
Jorge Gallardo Heck <luzzdeurano@gmail.com>
2025-05-23 19:01:05 -0400

    Update BankBostonMain.java

--- BankBostonMain.java
+++ BankBostonMain.java
@@ -17,19 +17,52 @@ public class BankBostonMain {
             switch (opcionMenu){
                 case 1:
                     Cliente.registroCliente(cliente);
+
+                    System.out.println("-------------------------");
+                    System.out.println("Cuenta Corriente Creada");
+                    System.out.println("Cuenta Corriente Nª: " + cliente.getCuenta().getNumeroCuentaCorriente());
+                    System.out.println("Tu saldo actual es de: " + "$" + cliente.getCuenta().getSaldoCuentaCorriente() + " CLP");
+                    System.out.println("-------------------------");
+
+
+
                     break;
                 case 2:
                     Cliente.impresionDatosCliente(cliente, cuenta);
                     break;
                 case 3:
-                    Cuenta.depositar();
+                    Scanner scannerDepositar = new Scanner(System.in);
+                    if (Administracion.listaCliente.size() == 0) {
+                        System.out.println("Primero debes ser cliente de Bank Boston");
+                    }else {
+                        System.out.println("Ingresa tu rut");
+                        String rut = scannerDepositar.nextLine();
+                        cuenta.depositar(rut);
+                    }
                     break;
                 case 4:
-                    Cuenta.giro();
+                    Scanner scannerGiro=new Scanner(System.in);
+
+                    if (Administracion.listaCliente.size() == 0) {
+                        System.out.println("Primero debes ser cliente de Bank Boston");
+                    }else {
+                        System.out.println("Ingresa tu rut");
+                        String rut = scannerGiro.nextLine();
+                        cuenta.girar(rut);
+                    }
                     break;
                 case 5:
-                    Cuenta.saldo();
+                    Scanner scannerSaldo = new Scanner(System.in);
+                    if(Administracion.listaCliente.size()==0){
+                        System.out.println("Primero debes ser cliente de Bank Boston");
+                    }else {
+                        System.out.println("Ingresa tu rut");
+                        String rut = scannerSaldo.nextLine();
+                        cuenta.saldoCuenta(rut);
+                    }
                     break;
+
+
             }
 
         }while(opcionMenu<6 && opcionMenu !=0);
